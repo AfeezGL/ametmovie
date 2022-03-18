1bfba3239206350b859d4b0d1f446a3a92409860
Afeez G Lawal <afeezlg@gmail.com>
2022-03-18 23:38:10 +0100

    started implementing see all functionality on movie categories

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,29 +1,68 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import star from "../images/star.svg";
 
 const Category = ({ category }) => {
+  const dialogRef = useRef();
+
   return (
     <div className="category">
       <div className="category-header">
         <p>{category.name}</p>
-        <span>see all</span>
+        <button
+          onClick={() => {
+            dialogRef.current.showModal();
+          }}
+        >
+          see all
+        </button>
       </div>
 
       <div className="cards">
-        {category.movies.map((anime) => (
-          <Link to={`/watch/${anime.data.videoId}`} key={anime.id}>
+        {category.movies.map((movie) => (
+          <Link to={`/watch/${movie.data.videoId}`} key={movie.id}>
             <figure className="card">
-              <img src={anime.data.imgUrl} alt="movie screenshot" />
-              <figcaption className="white">{anime.data.title}</figcaption>
+              <img src={movie.data.imgUrl} alt="movie screenshot" />
+              <figcaption className="white">{movie.data.title}</figcaption>
               <div className="ratings">
                 <img src={star} alt="Star icon" />
-                <span>{anime.data.rating}</span>
+                <span>{movie.data.rating}</span>
               </div>
             </figure>
           </Link>
         ))}
       </div>
+
+      <dialog ref={dialogRef}>
+        <header>
+          <button
+            onClick={() => {
+              dialogRef.current.close();
+            }}
+          >
+            <i className="fa-solid fa-xmark"></i>
+          </button>
+        </header>
+
+        {category.movies.map((movie) => (
+          <Link to={`/watch/${movie.data.videoId}`} key={movie.id}>
+            <div className="popup-item">
+              <img src={movie.data.imgUrl} alt="movie screenshot" />
+              <div>
+                <h4 className="white">{movie.data.title}</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Neque, optio!
+                </p>
+                <div className="ratings">
+                  <img src={star} alt="Star icon" />
+                  <span>{movie.data.rating}</span>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </dialog>
     </div>
   );
 };
